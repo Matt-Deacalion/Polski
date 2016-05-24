6bc28cb4704fe7ba0182612e702f78837c349206
Matt Deacalion Stevens <matt@dirtymonkey.co.uk>
2016-05-24 20:56:11 +0100

    Add missing dependency: `peewee`

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
     install_requires=[
         'docopt>=0.6.2',
         'fuzzywuzzy>=0.10.0',
+        'peewee>=2.8.1',
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
